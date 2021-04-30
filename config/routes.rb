04220f428e47c938f515324419d54cef20ff6c73
EC2 Default User <ec2-user@ip-172-31-29-122.us-east-2.compute.internal>
2021-04-30 12:37:31 +0000

    ファーストコミット

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get 'topics/new'
+  get 'sessions/new'
   root 'pages#index'
   get 'pages/help'
   get 'pages/link'
+  get 'users/new'
+  
+  resources :users
+  resources :topics
+  
+  get 'login', to:'sessions#new'
+  post 'login', to:'sessions#create'
+  delete 'logout', to:'sessions#destroy'
 end
