5c13c42b0c48a004c9b6831c4b94ae8e16eebe3f
EC2 Default User <ec2-user@ip-172-31-29-122.us-east-2.compute.internal>
2021-05-10 14:17:50 +0000

    コメント機能追加完了

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,6 @@
 class TopicsController < ApplicationController
   def index
     @topics = Topic.all.includes(:favorite_users)
-    @comment = Comment.new
   end
   def new
     @topic = Topic.new
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,5 @@
 class Comment < ApplicationRecord
-  validates :user_id, presence: true
   validates :content, presence: true
-  validates :topic_id, presence: true
   belongs_to :user
   belongs_to :topic
 end
